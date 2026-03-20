1d0be9e10f35614d0d8dbe34eced6d0bef13a9ca
murakami567 <murakami@gusk.jp>
2026-03-20 15:51:18 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -11,12 +11,12 @@ def root():
 @app.get("/tasks/today")
 def get_today_tasks():
     today = date.today().isoformat()
+
     res = (
         supabase.table("cleaning_tasks")
         .select("*")
         .eq("task_date", today)
-        .order("property_name")
-        .order("room_name")
         .execute()
     )
+
     return res.data
